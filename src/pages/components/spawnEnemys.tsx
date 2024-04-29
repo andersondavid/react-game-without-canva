b2d98eab2f05074b2e979b5993141cf9fa4ccc4a
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-29 15:49:54 -0300

    feat: separate spawn enemys

---
+++ src/pages/components/spawnEnemys.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Enemy from "./enemy";
+import styles from "./styles/SpawnEnemys.module.css";
+
+export default function SpawnEnemys() {
+  return (
+    <div className={styles.enemyZone}>
+      <Enemy id={1} />
+      <Enemy id={2} />
+      <Enemy id={3} />
+      <Enemy id={4} />
+      <Enemy id={5} />
+      <Enemy id={6} />
+      <Enemy id={7} />
+      <Enemy id={8} />
+      <Enemy id={9} />
+      <Enemy id={10} />
+      <Enemy id={11} />
+      <Enemy id={12} />
+      <Enemy id={13} />
+      <Enemy id={14} />
+      <Enemy id={15} />
+      <Enemy id={16} />
+      <Enemy id={17} />
+      <Enemy id={18} />
+      <Enemy id={19} />
+      <Enemy id={20} />
+      <Enemy id={21} />
+      <Enemy id={22} />
+      <Enemy id={23} />
+      <Enemy id={24} />
+      <Enemy id={25} />
+      <Enemy id={26} />
+      <Enemy id={27} />
+      <Enemy id={28} />
+      <Enemy id={29} />
+      <Enemy id={30} />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Head from "next/head";
 //import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Enemy from "./components/enemy";
 import { useBulletContext } from "@/api/bulletContext";
-import SpawnBullets from "./components/spawnbullets"; './components/spawnBullets'
+import SpawnBullets from "./components/spawnbullets";import SpawnEnemys from "./components/spawnEnemys";
+ './components/spawnBullets'
 
 //const inter = Inter({ subsets: ["latin"] });
 
@@ -27,38 +29,7 @@ export default function Home() {
         <h2>{bulletsList.length}</h2>
         <div className={styles.space}>
           <div className={styles.enemySpace}>
-            <div className={styles.enemyZone}>
-              <Enemy id={1} />
-              <Enemy id={2} />
-              <Enemy id={3} />
-              <Enemy id={4} />
-              <Enemy id={5} />
-              <Enemy id={6} />
-              <Enemy id={7} />
-              <Enemy id={8} />
-              <Enemy id={9} />
-              <Enemy id={10} />
-              <Enemy id={11} />
-              <Enemy id={12} />
-              <Enemy id={13} />
-              <Enemy id={14} />
-              <Enemy id={15} />
-              <Enemy id={16} />
-              <Enemy id={17} />
-              <Enemy id={18} />
-              <Enemy id={19} />
-              <Enemy id={20} />
-              <Enemy id={21} />
-              <Enemy id={22} />
-              <Enemy id={23} />
-              <Enemy id={24} />
-              <Enemy id={25} />
-              <Enemy id={26} />
-              <Enemy id={27} />
-              <Enemy id={28} />
-              <Enemy id={29} />
-              <Enemy id={30} />
-            </div>
+            <SpawnEnemys />
           </div>
           <div className={styles.heroSpace}></div>
           <SpawnBullets />
